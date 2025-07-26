dab565479553537f26bd9b7d0c757fdefb55b5fd
samikayaa <samikaya89@gmail.com>
2025-07-26 17:25:16 +0300

    c

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -8,20 +8,19 @@ export default function CartContextProvider({ children }) {
 
     const [cart, setCart] = useState([]);
 
-
     const addItem = (item) => {
         // verilen itemi sepete ekle
-        setCart([...cart, item])
+        setCart([...cart, item]);
     };
 
     const removeItem = (id) => {
         // verilen itemi sepetten çıkar......
-
-    }
+        setCart(cart.filter(item => item.id !== id))
+    };
 
     return (
-        <CartContext.Provider value={{ cart }}>
+        <CartContext.Provider value={{ cart, addItem, removeItem }}>
             {children}
         </CartContext.Provider>
-    )
+    );
 }
